ece2c8274f18b09b5318852af0ec979b496f3cb2
Devin Lin <devin.lin@stonybrook.edu>
2023-11-02 17:26:27 -0400

    Initialization of pygame

---
+++ steps.py
@@ -0,0 +1,14 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
+    clock.tick(60)
