df6728d3d5c6080f49799aac2b2bf2e229829ceb
Devin Lin <devin.lin@stonybrook.edu>
2023-11-02 18:24:33 -0400

    Detecting Collisions

--- steps.py
+++ steps.py
@@ -15,7 +15,7 @@ test_font = pygame.font.Font("font/open-sans/OpenSans-Regular.ttf", 50)
 text_surface = test_font.render("My Game", False, "Black")
 
 sammi_surface = pygame.image.load("graphics/eboard_Faces/sammi_head_icon.png").convert_alpha()
-sammi_x_pos = 600
+sammi_rect = sammi_surface.get_rect(topleft = (600, 240))
 
 player_surf = pygame.image.load("graphics/player/hopper_player.png")
 player_rect = player_surf.get_rect(midbottom = (100, screen.get_height() - ground_surface.get_height()))
@@ -29,15 +29,18 @@ while True:
     screen.blit(ground_surface, (0, screen.get_height() - ground_surface.get_height()))
     screen.blit(text_surface, (300, 50))
 
-    sammi_x_pos -= 3
-    if sammi_x_pos <= -100:
-        sammi_x_pos = 800
-    screen.blit(sammi_surface, (sammi_x_pos, 240))
+    sammi_rect.x -= 3
+    if sammi_rect.x <= -100:
+        sammi_rect.x = 800
+    screen.blit(sammi_surface, sammi_rect)
 
     player_rect.left += 3
-    print(player_rect.right)
     if player_rect.right >= 800:
         player_rect.left = 0
     screen.blit(player_surf, player_rect)
+
+    if player_rect.colliderect(sammi_rect):
+        print("hii")
+
     pygame.display.update()
     clock.tick(60)
