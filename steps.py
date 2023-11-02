4326c5e476f38e0277f429e01e47bb0eda5a798e
Devin Lin <devin.lin@stonybrook.edu>
2023-11-02 17:41:21 -0400

    Basic animation

--- steps.py
+++ steps.py
@@ -14,6 +14,9 @@ ground_surface = pygame.image.load("graphics/ground.png").convert()
 test_font = pygame.font.Font("font/open-sans/OpenSans-Regular.ttf", 50)
 text_surface = test_font.render("My Game", False, "Black")
 
+sammi_surface = pygame.image.load("graphics/eboard_Faces/sammi_head_icon.png").convert_alpha()
+sammi_x_pos = 600
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -22,5 +25,10 @@ while True:
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, screen.get_height() - ground_surface.get_height()))
     screen.blit(text_surface, (300, 50))
+
+    sammi_x_pos -= 3
+    if sammi_x_pos <= -100:
+        sammi_x_pos = 800
+    screen.blit(sammi_surface, (sammi_x_pos, 240))
     pygame.display.update()
     clock.tick(60)
