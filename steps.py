d75c3c7514d907e29e8a439604248492de5700af
Devin Lin <devin.lin@stonybrook.edu>
2023-11-03 05:06:54 -0400

    Game over screen

--- steps.py
+++ steps.py
@@ -13,14 +13,19 @@ ground_surface = pygame.image.load("graphics/ground.png").convert()
 
 test_font = pygame.font.Font("font/open-sans/OpenSans-Regular.ttf", 50)
 start_time = 0
+score = 0
 def display_score():
     current_time = int(pygame.time.get_ticks()/1000) - start_time
     score_surf = test_font.render(f"Score {current_time}", False, (64,64,64))
     score_rect = score_surf.get_rect(center = (400, 50))
     screen.blit(score_surf, score_rect)
 
+    return current_time
+
 sammi_surface = pygame.image.load("graphics/eboard_Faces/sammi_head_icon.png").convert_alpha()
 sammi_rect = sammi_surface.get_rect(topleft = (600, 240))
+sammi_surface = pygame.transform.scale(sammi_surface, (sammi_surface.get_width() * 0.7, sammi_surface.get_height() * 0.7))
+sammi_rect = sammi_surface.get_rect(topleft = (600,260))
 
 player_surf = pygame.image.load("graphics/player/hopper_player.png")
 player_rect = player_surf.get_rect(midbottom = (100, screen.get_height() - ground_surface.get_height()))
@@ -28,6 +33,17 @@ player_rect = player_surf.get_rect(midbottom = (100, screen.get_height() - groun
 player_gravity = 0
 
 game_active = True
+
+hopper_screen = pygame.image.load('graphics/hopper_intro.png').convert_alpha()
+hopper_screen = pygame.transform.scale(hopper_screen, (hopper_screen.get_width()/2.5, hopper_screen.get_height() / 2.5))
+hopper_screen_rect = hopper_screen.get_rect(center = (400, 200))
+
+game_name = test_font.render("Hopper Hops", False, '#E85F5C')
+game_name_rect = game_name.get_rect(center = (400, 80))
+
+game_message = test_font.render("Press space to run", False, "Black")
+game_message = pygame.transform.scale(game_message, (game_message.get_width() * 0.6, game_message.get_height()*0.6))
+game_message_rect = game_message.get_rect(center = (400, 320))
 
 while True:
     for event in pygame.event.get():
@@ -48,7 +64,7 @@ while True:
     if game_active:
         screen.blit(sky_surface, (0,0))
         screen.blit(ground_surface, (0, screen.get_height() - ground_surface.get_height()))
-        display_score()
+        score = display_score()
 
         sammi_rect.x -= 3
         if sammi_rect.x <= -100:
@@ -72,7 +88,17 @@ while True:
         if player_rect.colliderect(sammi_rect):
             game_active = False
     else:
-        screen.fill("Yellow")
+        screen.fill("#086375")
+        screen.blit(hopper_screen, hopper_screen_rect)
+        screen.blit(game_name, game_name_rect)
+
+        score_message = test_font.render(f'Your Score: {score}', False, "Black")
+        score_message_rect = score_message.get_rect(center = (400, 300))
+
+        if score == 0:
+            screen.blit(game_message, game_message_rect)
+        else:
+            screen.blit(score_message, score_message_rect)
 
     pygame.display.update()
     clock.tick(60)
